a4db84d2b77647175533ce676a1fe9eae0d49935
khaitk <khaitkdev@gmail.com>
2024-05-07 21:15:04 +0700

    Create Order

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 const amqp = require("amqplib");
+const mongoose = require("mongoose");
+const Order = require("./models/order.entity");
 
-const queue = "product_inventory";
-const text = {
-  item_id: "macbook",
-  text: "This is a sample message to send receiver to check the ordered Item Availablility",
-};
+mongoose.connect("", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log("Order-Service Connected to MongoDB"))
+  .catch((e) => console.log(e));
 
-(async () => {
-  let connection;
-  try {
-    connection = await amqp.connect("amqp://localhost");
-    const channel = await connection.createChannel();
-
-    await channel.assertQueue(queue, { durable: false });
-    channel.sendToQueue(queue, Buffer.from(JSON.stringify(text)));
-    console.log(" [x] Sent '%s'", text);
-    await channel.close();
-  } catch (err) {
-    console.warn(err);
-  } finally {
-    if (connection) await connection.close();
+  async function connectToRabbitMQ() {
+    const amqpServer = "amqp://localhost";
+    connection = await amqp.connect(amqpServer);
+    channel = await connection.createChannel();
+    await channel.assertQueue("order-service-queue");
   }
-})();
+
+connectToRabbitMQ().then(() => {
+  channel.consume("order-service-queue", (data) => {
+    // order service queue listens to this queue
+    console.log("Consumed from order-service-queue");
+    const { products } = JSON.parse(data.content);
+    const newOrder = createOrder(products);
+    channel.ack(data);
+    channel.sendToQueue(
+      "product-service-queue",
+      Buffer.from(JSON.stringify(newOrder))
+    );
+  });
+});
+
+createOrder = (products) => {
+  let total = 0;
+  products.forEach((product) => {
+    total += product.price;
+  });
+
+  const order = new Order({
+    products,
+    total,
+  });
+  order.save();
+  return order;
+};
